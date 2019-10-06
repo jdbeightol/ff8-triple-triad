b1ebb2b4973a9f7193c2c64ed7695b31caf6fe68
Jeremy Beightol <jdbeightol@gmail.com>
2019-10-06 18:14:31 -0400

    minor tweaks

--- seeds.py
+++ seeds.py
@@ -7,7 +7,7 @@ import rules
 def process_ranges(func, values):
     for v in values:
         if v == 'all':
-            for i in range(1, len(seed_data)):
+            for i in range(1, len(seed_data) + 1):
                 func(seed_data[i], i)
         elif '-' in v:
             r = v.split('-')
@@ -22,8 +22,8 @@ def process_ranges(func, values):
             func(seed_data[int(v)], int(v))
 
 
-a = argparse.ArgumentParser(description='Triple Triad RNG manipulation utility.')
-a.add_argument('command', choices=['print', 'get_all', 'get_rules', 'get_abolish', 'get_adopt', 'get_table'], help='command')
+a = argparse.ArgumentParser(description='RNG seed data utility.')
+a.add_argument('command', choices=['print', 'get', 'get_all', 'get_rules', 'get_abolish', 'get_adopt'], help='command')
 a.add_argument('ranges', nargs='+', help='ranges of seeds relevant to the action')
 a.add_argument('--random-file', '-f', default='random.dat', help='filename of random data to load')
 args = a.parse_args()
@@ -34,21 +34,21 @@ seed_data = rand.load(args.random_file)
 # Print will output the raw seed numbers.
 if args.command == 'print':
     # All subcommands for print will be seed numbers and ranges.
-    process_ranges(print, args.ranges)
+    process_ranges(lambda x,y: print(x), args.ranges)
 
 # Get rules allows us to see the rule names.
 elif args.command == 'get_rules':
-    process_ranges(lambda x : print(rules.get_rule(x)), args.ranges)
+    process_ranges(lambda x,y : print(rules.get_rule(x)), args.ranges)
 
 # Get rules allows us to see the rule names.
 elif args.command == 'get_abolish':
-    process_ranges(lambda x : print('yes' if rules.can_abolish(x) else 'no'), args.ranges)
+    process_ranges(lambda x,y : print('yes' if rules.can_abolish(x) else 'no'), args.ranges)
 
 # Get rules allows us to see the rule names.
 elif args.command == 'get_adopt':
-    process_ranges(lambda x : print('yes' if rules.can_abolish(x) else 'no'), args.ranges)
+    process_ranges(lambda x,y : print('yes' if rules.can_abolish(x) else 'no'), args.ranges)
 
-elif args.command == 'get_table':
+elif args.command == 'get':
     seed_list = []
     value_list = []
     rule_list = []
