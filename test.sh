b475e61051286075f0a9c065581f79e6d764380d
Jeremy Beightol <jdbeightol@gmail.com>
2019-10-05 23:23:49 -0400

    added more tests

--- test.sh
+++ test.sh
@@ -2,5 +2,9 @@
 
 ./triple-triad.py abolish same -c open -r same
 ./triple-triad.py spread open -c open -r same
+./triple-triad.py spread Open -c Open -r Same -s 50
+./triple-triad.py abolish Same -c Open -r Same -q -x
+./triple-triad.py spread Open -c Open -r Same -x
+./triple-triad.py abolish Same -c Open -r Same -q
 ./triple-triad.py abolish Same -c Open -r Same
 ./triple-triad.py spread Open -c Open -r Same
